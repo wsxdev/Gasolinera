379f3736b8cd2f6651790ee042bef2f78d8cb26a
wsxdev <williamsxdev@gmail.com>
2025-06-09 22:44:49 -0500

    Agregar archivos de configuración y estructura inicial del proyecto Gasolinera

---
+++ src/main/java/grafo/NodoGrafo.java
@@ -0,0 +1,5 @@
+package grafo;
+
+public interface NodoGrafo {
+    String getClave();
+}
---
+++ src/main/java/grafo/Principal.java
@@ -0,0 +1,8 @@
+package grafo;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
